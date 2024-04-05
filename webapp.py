bac1888e03bb60a8d0d5dfe614741c23bed39f60
JamisonSavelleno <jamison2006@icloud.com>
2024-04-05 13:58:14 -0700

    stuff

--- webapp.py
+++ webapp.py
@@ -19,6 +19,14 @@ def render_fact():
     county = county_most_under_18(state)
     fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
     return render_template('home.html', state_options=states, funFact=fact)
+
+@app.route('/showFact2')
+def render_fact2():
+    states = get_state_options()
+    state = request.args.get('state')
+    vets = get_most_vets(state)
+    fact2 = "In " + state + ", the county with the most veterans is " + county + "."
+    return render_template('home.html', state_options=states, funFact2=fact2)
     
 def get_state_options():
     """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
@@ -32,6 +40,19 @@ def get_state_options():
     for s in states:
         options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
+    
+def get_most_vets(state):
+    """Return the name of a county in the given state with the highest number of vets."""
+    with open('demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    highest=0
+    county = ""
+    for c in counties:
+        if c["State"] == state:
+            if c["Miscellaneous"]["Veterans"] > highest:
+                highest = c["Miscellaneous"]["Veterans"]
+                county = c["County"]
+    return county
 
 def county_most_under_18(state):
     """Return the name of a county in the given state with the highest percent of under 18 year olds."""
